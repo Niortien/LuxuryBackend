b2f611615cffe516975fbf4adbfb65e11efd3eb6
Niortien <Niortiencoulibaly2001@gmail.com>
2025-05-01 11:28:12 +0000

    up

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
   // CONNEXION SERVEUR
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 
 void bootstrap();
